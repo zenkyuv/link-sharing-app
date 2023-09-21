5b0907b9be532b969f593adfd876597abec833fa
zenkyu <przemekgg2002@gmail.com>
2023-09-21 20:21:11 +0200

    add: context

---
+++ src/context.ts
@@ -0,0 +1,32 @@
+import {Link, Profile, ProfileDetails} from "./types"
+
+export class Context {
+
+	profile: Profile = {
+		first_name: "",
+		last_name: "",
+		image_url: "",
+		email: "",
+		links: []
+	}
+
+	constructor() {}
+
+	set image_url(url: string) {
+		this.profile.image_url = url
+	}
+
+	set profile_links(links: Link[]) {
+		this.profile.links = links
+	}
+
+	set profile_details({first_name, last_name, email}: ProfileDetails) {
+		this.profile = {
+			...this.profile,
+			first_name,
+			last_name,
+			email
+		}
+	}
+
+}
