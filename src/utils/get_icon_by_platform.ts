9c6a6e4331702f0ae0c4e7065b2ccb40f9d8836b
zenkyu <przemekgg2002@gmail.com>
2023-09-30 02:27:01 +0200

    tweak: import path

--- src/utils/get_icon_by_platform.ts
+++ src/utils/get_icon_by_platform.ts
@@ -1,4 +1,4 @@
-import {Platform} from "../../../types"
+import {Platform} from "../types.js"
 
 export function get_icon_by_platform(platform: Platform) {
 	return `./assets/images/icon-${platform.split(" ").join("").toLowerCase()}.svg`
