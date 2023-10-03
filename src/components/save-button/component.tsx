41011a8822c50ce02f0111d7e1fe62170f9d7965
zenkyu <przemekgg2002@gmail.com>
2023-10-04 01:42:15 +0200

    add: non-modal dialog

--- src/components/save-button/component.tsx
+++ src/components/save-button/component.tsx
@@ -1,11 +1,25 @@
 import styles from './style.module.css'
 
+import { useRef } from 'react'
+
 export default function SaveButton() {
 
+const dialogRef = useRef<HTMLDialogElement | null>(null)
+
+	const show_timed_modal = () => {
+		if(dialogRef && !dialogRef.current?.open) {
+			dialogRef.current?.show()
+			setTimeout(() => dialogRef.current!.close(), 4000)
+		}
+	}
+
 return (
+	<>
 		<div className={styles.buttonBox}>
 			<div className={styles.buttonBorder}></div>
-			<button>Save</button>
+			<button onClick={show_timed_modal}>Save</button>
 		</div>
+		<dialog ref={dialogRef}><p><img src="./assets/images/icon-changes-saved.svg" />Your changes have been successfully saved!</p></dialog>
+	</>
 	)
 }
